8d19014f3df6cdcb732d297742c7b93a042f9170
Denis De Mesmaeker <denis.demesmaeker@gmail.com>
2020-03-06 16:32:55 +0100

    added port usage for the board forgetmenot

--- modules/common/def.h
+++ modules/common/def.h
@@ -8,20 +8,46 @@
 
 #include <avr/io.h>
 
-// PORT B
-#define BLK      PIN0_bm
-#define DBG      PIN1_bm
-#define LED_R    PIN1_bm
-#define CS_LCD   PIN2_bm
-#define DC       PIN3_bm
-#define RST      PIN4_bm
-#define LED_B    PIN5_bm
-#define LED_G    PIN6_bm
-#define OUT1     PIN7_bm
+// PORT A
+#define MOSI       PIN1_bm
+#define MISO       PIN2_bm
+#define SCK        PIN3_bm
+#define CS_RFM     PIN4_bm
+#define DIO0       PIN5_bm
+#define DIO1       PIN6_bm
+#define OUT2       PIN7_bm
 
-#define PORT_LED PORTB
-#define PORT_DBG PORTB
-#define PORT_LCD PORTB
+#define PORT_SPI   PORTA
+#define PORT_RFM   PORTA
+
+// PORT B
+#define BLK        PIN0_bm
+#define DBG        PIN1_bm
+#define LED_R      PIN1_bm
+#define CS_LCD     PIN2_bm
+#define DC         PIN3_bm
+#define RST        PIN4_bm
+#define LED_B      PIN5_bm
+#define LED_G      PIN6_bm
+#define OUT1       PIN7_bm
+
+#define PORT_LED   PORTB
+#define PORT_DBG   PORTB
+#define PORT_LCD   PORTB
+
+// PORT C
+#define TEMP1      PIN0_bm
+#define MOIST      PIN1_bm
+#define TEMP2      PIN2_bm
+#define HALL       PIN3_bm
+#define MULTI      PIN4_bm
+#define TOUCH      PIN5_bm
+
+#define PORT_MOIST PORTC
+#define PORT_TEMP  PORTC
+#define PORT_HALL  PORTC
+#define PORT_TOUCH PORTC
+#define PORT_HALL  PORTC
 
 inline static void led_setup() {
   PORT_LED.DIRSET = LED_G;  // output
